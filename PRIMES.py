1292892e341d18b381d596523692cefaba7f4d3d
tjf801 <47402907+tjf801@users.noreply.github.com>
2019-09-30 20:06:30 -0400

    Add files via upload

--- PRIMES.py
+++ PRIMES.py
@@ -1,4 +1,4 @@
-from math import floor, ceil, sqrt, gcd, log2
+from math import floor, ceil, sqrt, gcd, log2, log10
 from decimal import *
 
 def TestPerfectPower(n):
@@ -63,6 +63,11 @@ def gcd_extended(a,b):
 		if(b == 0):
 			return (a, a1*aneg, b1*bneg)
 
+def modinverse(a, n):
+	(gcd, ax, bn) = gcd_extended(a, n)
+	if gcd!= 1: raise ValueError("Modular inverse does not exist")
+	return ax % n
+
 def lcm(a, b):
 	c = a * b
 	c = c / gcd(a, b)
@@ -96,12 +101,9 @@ def λ(n):
 			#TODO
 			pass
 
-def modinverse(a, n):
-	(gcd, ax, bn) = gcd_extended(a, n)
-	if gcd!= 1: raise ValueError("Modular inverse does not exist")
-	return ax % n
-
-def AKS_prime_check(n):
+def isPrime(n):
+	#Basic AKS Primality Check, no fancyness needed
+	#runs in O(log2(n)^5)
 	
 	if n%2 == 0: return False
 	
@@ -110,8 +112,11 @@ def AKS_prime_check(n):
 	r = get_r(n)
 	
 	a = r
+	#for (a=r; a>1 && a<n; a--;)
 	while a > 1 and a < n:
-		if gcd(a, n) != 1:
+		g = gcd(a, n)
+		if g != 1:
+			print("factor: " + str(g))
 			return False
 		a -= 1
 	
@@ -121,6 +126,7 @@ def AKS_prime_check(n):
 	
 	bound = floor(sqrt(φ(r)) * log2(n))
 	
+	#for (a=1; a<=bound; a++;)
 	while a <= bound:
 		if pow(a, n, n) - a != 0:
 			return False
@@ -129,3 +135,99 @@ def AKS_prime_check(n):
 	return True
 
 
+#Sieve prime check adapted from https://github.com/smanikar/primality-testing/blob/master/proposal/src/AKS.java
+def sieveEratos():
+	global sieveSize 
+	sieveSize = 1000000
+	global sieveArray 
+	sieveArray = [False] * (sieveSize+1)
+	sieveArray[1] = True
+	i = 2
+	while i < sqrt(sieveSize):
+		if sieveArray[i] is not True:
+			j = i * i
+			while j <= sieveSize: sieveArray[j] = True; j+=i
+		i+=1
+
+def checkSievePrime(val):
+	return not sieveArray[int(val)]
+
+def largestFactor(num):
+	i = int(num)
+	if i==1: return i
+	while i > 1:
+		while sieveArray[i]:
+			i-=1
+		if num%i == 0:
+			return i
+		i-=1
+	return num
+
+def mPower(x, y, n):
+	return pow(x, y, n)
+
+def sieve_prime_check(n):
+	log = floor(log10(n))
+	
+	sieveEratos()
+	
+	if TestPerfectPower(n): return False
+	r = Decimal(2)
+	x = r
+	
+	
+	#for (r=2; r<n; r++;)
+	while r<n:
+		if gcd(r, n)!=1: print("checking r", r); return False
+		
+		if checkSievePrime(r):
+			print("checking r", r, end="\r")
+			quot = largestFactor(r - 1)
+			divisor = (r - 1) / quot
+			tm = 4 * (sqrt(r)) * log;
+			powOf = mPower(n, divisor, r);
+			if quot>=tm and powOf != 1: break
+		
+		r += 1
+	
+	print("found r of " + str(r))
+	
+	aLimit = ceil(2 * sqrt(r) * log)
+	
+	aCounter = Decimal(1)
+	#for (aCounter = 1; aCounter < aLimit; aCounter++;)
+	while aCounter < aLimit:
+		print("checking", aCounter, end="\r")
+		aBigNum = aCounter
+		leftH = (mPower(x-aBigNum, n, n))%n
+		rightH = (mPower(x, n, n)-aBigNum) % n
+		if leftH != rightH: print("checking", aCounter); return False
+		aCounter+=1
+	
+	print("\n")
+	
+	return True
+
+def fasterfactor(n):
+	i = Decimal(2)
+	p = Decimal(1)
+	k = n
+	factors = []
+	if sieve_prime_check(n): factors.append(n)
+	while (p*p <= k and i*i <= k):
+		if n % i == 0:
+			n = n / i
+			p = p * i
+			factors.append(i)
+			if p==k or sieve_prime_check(n): break
+		else:
+			i = i + 1
+	if p != k:
+		factors.append(int(k/p))
+	
+	return factors
+
+
+#n = Decimal(input())
+#getcontext().prec = 2*len(str(n))
+#print(sieve_prime_check(n))
